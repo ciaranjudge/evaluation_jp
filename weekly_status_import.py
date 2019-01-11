64a6bb206f5335454cd6e29abb19c8259eee8f66
Ciaran Judge <CiaranJudge@welfare.irlgov.ie>
2019-01-11 18:58:09 +0000

    Chunking file read

--- weekly_status_import.py
+++ weekly_status_import.py
@@ -1,56 +1,97 @@
-# %% [markdown]
-# # Weekly Status
-
 # %%
-import pandas as pd              # python package for dataframes
+import pandas as pd  # python package for dataframes
 from datetime import datetime
+
 pd.options.display.max_columns = None
 pd.options.display.max_rows = None
 
 # %%
-in_df = pd.read_csv(
-    '\\\\cskma0294\\F\\Evaluations\\JobPath\\\Quarterly_status\\WeeklyStatus.zip')
-in_df = in_df.drop(['Purge_ind'], axis=1)
-in_df.head()
+status_summary = pd.read_csv("data\status_summary.csv")
 
 # %%
-df = pd.melt(in_df, id_vars=['ppsn'], var_name='date', value_name='status')
+in_csv = "\\\\cskma0294\\F\\Evaluations\\JobPath\\\Quarterly_status\\WeeklyStatus.zip"
+
 
 # %%
-df = df[df['date'] != ''].dropna()
-df['date'] = [x.strip('StatusEnd_') for x in df['date']]
-# %%
-df['date'] = pd.to_datetime(df['date'], infer_datetime_format=True).dt.date
+chunksize = 10 ** 4
+n_chunks = 1
+csv_parser = pd.read_csv(in_csv, chunksize=chunksize, nrows=chunksize * n_chunks)
 
 # %%
-max_simultaneous = df['status'].str.count('\+').max() + 1
+
+for chunk_index, df in enumerate(csv_parser):
+    # Remove unneeded column
+    df = df.drop(["Purge_ind"], axis=1)
+
+    # Reshape by melting to long format, converting columns to date values
+    df = pd.melt(df, id_vars=["ppsn"], var_name="date", value_name="status")
+    
+    # Tidy status column and remove statuses that are just '.'
+    df["status"] = df["status"].str.lstrip(to_strip=".\+")
+    df = df[df["status"] != "."]
+
+    # Remove any records with no date and drop nulls (there's lots of nulls!)
+    df = df[df["date"] != ""].dropna()
+
+    # Remove 'StatusEnd_' from start of dates
+    df["date"] = df["date"].str.lstrip("StatusEnd_")
+    # Convert dates to datetime format
+    df["date"] = pd.to_datetime(df["date"], infer_datetime_format=True).dt.date
+
+    # Append current chunk to overall output
+    df['status'] = df['status'].astype('category')
+    print(chunk_index)
+    if chunk_index == 0:
+        out_df = df
+    else:
+        out_df = out_df.append(df)
 
 # %%
-status_cols = ['s' + str(i) for i in range(max_simultaneous)]
-df[status_cols] = df['status'].str.split(pat='\+', expand=True)
+ # Unpack status column
+mask = out_df['status'].str.contains('\+')
+process_df = out_df[mask]
+result_df = out_df[~mask]
 
+max_sep = process_df['status'].str.count('\+').max() + 1
+status_cols = ['s' + str(i) for i in range(max_sep)] 
+process_df[status_cols] = process_df['status'].str.split(pat='\+', expand=True)
+
+# process_df = process_df.drop('status', axis='columns')
+# for col in status_cols:
+#     append_df = process_df[['ppsn', 'date', col]].dropna()
+#     result_df = result_df.append(append_df)
+# result_df.describe()
+process_df.head()
+
+
+# %%
+out_df.info()
+
+# %%
+print([f"{c}: {df[c].dtype}" for c in out_df.columns])
+
+# %%
+pd.a(df["status"])
 # %%
 df.info()
 
 # %%
-df = df.drop('status', axis='columns')
+df = df.set_index(["ppsn", "date"])
 
 # %%
-df.info()
+# dates = df['date'].unique()
+df.index.get_level_values(1)
 
 # %%
-df = df.set_index(['ppsn', 'date'])
 
 # %%
-
-# %%
-df_dummies = (pd.get_dummies(df, prefix='', prefix_sep='')
+""" df_dummies = (pd.get_dummies(df, prefix='', prefix_sep='')
               .max(level=0, axis=1)
               .astype('bool')
               )
 
 # %%
-df_dummies.info()
+df_dummies.info() """
 """ 
 # %%
 df_with_dummies = pd.concat([df, df_dummies], axis='columns')
@@ -74,15 +115,7 @@ df_dummies.columns
  """
 
 # %%
-
-
-
-
-""" df_melt = pd.melt(df,
-                  id_vars=['ppsn', 'date'], 
-                  var_name='status', 
-                  value_name='flag'
-                  )
+df_melt = pd.melt(df, id_vars=["ppsn", "date"], var_name="status", value_name="flag")
 
 # %%
-df_melt.info() """
+df_melt.info()
