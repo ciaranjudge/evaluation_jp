f01e1b16a05df4849b18c81dca5934ba3122bdb2
ciaranjudge <judgeci@tcd.ie>
2019-01-21 20:20:15 +0000

    testing commit direction

--- weighting.py
+++ weighting.py
@@ -1,3 +1,5 @@
+
+
 # %%
 from datetime import datetime
 
@@ -5,10 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import pandas as pd
-import seaborn as sns
-from IPython.display import HTML, display
-
-from scipy import stats
+import seaborn as sns  # package for plotting
+from IPython.display import HTML, display  # Make tables pretty
 
 sns.set()
 
@@ -17,7 +17,7 @@ sns.set()
 # ## Import outcomes dataset and tidy up
 
 # %%
-df = pd.read_csv("data/jp_outcomes.zip")
+df = pd.read_csv("jp_outcomes.zip")
 
 
 # %%
@@ -32,151 +32,145 @@ for col in df.select_dtypes(include=numerics).columns:
 
 # %%
 # Tidy up T and C columns to make processing easier
-# Need to use multiindex!
-periods = {i: "period_"+ str(i) for i in range(1, 9)}
+periods = []
+for i in range(1, 9):
+    this_period = 'period_'+str(i)
+    df[this_period] = df['Group'+str(i)].str[:1]
+    periods.append(this_period)
 
-for i, period in periods.items():
-    df[period] = df["Group" + str(i)].str[:1]
-    cat_map = {'T': 1, 'C': 0, '0': np.NaN}
-    df[period] = df[period].map(cat_map)
-    # df[period] = df[period].astype('int')
-    previous_periods = list(periods.values())[:i-1]
-    df[period].loc[(df[period] == 1) & df[previous_periods].any()] = 0
-    df[period] = df[period].fillna(-1)
-    
-
-
-#     print(i+2, period)
-#     print(df[period].value_counts())
-#     # Eliminate earlier T people from later period C   
-#     for j in range(i + 2, max_period):
-#         later_period = "period_" + str(j)
-#         df[later_period].loc[df[this_period] == "T"] = '0'
-
-
-# %%
-
-
-# %%
-df.info(verbose=True)
-
-# %%
-period_list = list(periods.values())
-df[period_list].describe()
-
-# %%
-# Remove outliers
-numeric_cols = [
-    col for col in df.columns.tolist() if col.startswith(("earn_", "sw_pay_"))
-]
-
-# df1 = df[(np.abs(stats.zscore(df[numeric_cols])) < 5).all(axis=1)]
+# Eliminate earlier T people from later period C
+for i in range(1, 9):
+    for j in range(i+1, 9):
+        df['period_'+str(j)].loc[df['period_'+str(i)] == 'T'] = 0
 
 # %% [markdown]
 # ## Look - a group triangle!
 
-
-# %% [markdown]
-# ## Add weights
-# This is just a global sort but needs to be replaced by a per-group classification function
-# called at the start of the binning/weighting code
-
 # %%
-# Sort all records
-df = df.set_index(
-    [
-        # 'cluster',
-        "earn_tot_mean_1315",
-        "sw_pay_mean_1315",
-        "duration_days_0",
-        "id",
-    ]
-)
-df.sort_index(inplace=True)
-df.reset_index(inplace=True)
-df.index.name = "rank"
-# # np.sum(df1.index.duplicated())
-df.reset_index(inplace=True)
-df.set_index("id", inplace=True)
-
-# %%
-def add_weights(df, period):
-    # 1. Create scores
-    # Currently done above with global ranks but need to replace
-
-    # 2. Create bins based on scores
-    # Create a temporary df for T and C groups
-    df_T = df.loc[df[period] == "T"].copy()
-    df_C = df.loc[df[period] == "C"].copy()
-
-    # Split T group into equal sized bins
-    df_T["bin"], bins = pd.qcut(df_T["rank"], 100, retbins=True, labels=False)
-
-    # Put C group into T bins based on T bin edges
-    df_C["bin"] = pd.cut(df_C["rank"], bins, labels=range(len(bins) - 1))
-
-    # Exclude unassigned C group members to eliminate outliers
-    df_C = df_C.dropna(subset=["bin"])
-    df_C["bin"] = df_C["bin"].astype("int")
-
-    # 3. Add weights based on bins
-    # Create counts for T and C by bin
-    t_bin_counts = pd.Series(df_T.groupby("bin")["rank"].count(), name="t_bin_counts")
-    c_bin_counts = pd.Series(df_C.groupby("bin")["rank"].count(), name="c_bin_counts")
-    bin_counts = pd.concat([t_bin_counts, c_bin_counts], axis="columns")
-
-    # Divide T by C to get weights for C group
-    bin_counts["abs_weight"] = bin_counts["t_bin_counts"] / bin_counts["c_bin_counts"]
-    c_total = df_C.shape[0]
-    t_total = df_T.shape[0]
-    bin_counts["weight"] = bin_counts["abs_weight"] * c_total / t_total
-
-    # All Ts have weight = 1
-    df_T["weight"] = 1
-    df_T["abs_weight"] = 1
-
-    # Assign C weights based on weights in bin_counts dataframe
-    # Have to reset and then set index to avoid losing it!
-    df_C = df_C.reset_index()
-    df_C = df_C.merge(bin_counts[["abs_weight", "weight"]], how="inner", on="bin")
-    df_C = df_C.set_index("id")
-
-    # Append T and C dataframes together
-    out_df = df_T.append(df_C)
-    out_df = out_df[["weight", "abs_weight", 'bin']]
-
-    return out_df
-
-
-# %%
-for period in periods:
-    w_df = add_weights(df, period)
-    w_df.columns = [period + "__" + str(col) for col in w_df.columns]
-    df = pd.concat([df, w_df], axis="columns")
-
-df.info(verbose=True)
-
-# %%
-grouped = df.groupby(periods)["rank"].count()
+grouped = df.groupby(periods)['id'].count()
 grouped.sort_index(ascending=False, inplace=True)
 grouped
 
-# %%
-for period in periods:
-    aw_col = period + "__abs_weight"
-    print(df.groupby(period)[aw_col].sum())
+
+# %% [markdown]
+# ## Add weights
 
 
 # %%
-df[periods].any(axis='columns').head()
+# def create_ranks (df, sorting_function):
+"""
+Adds a ranking column to a dataframe based on a sorting function
+
+Given a ranking function and a dataframe with the columns needed for that
+ranking function, this function returns a ranking of all the rows.
+
+Parameters
+----------
+df : DataFrame
+    Dataframe with unique index and columns needed for rank_function
+sorting_function : function
+    A sorting algorithm which returns a sorted version of a given dataframe
+
+Returns
+-------
+rank : Series
+    A pandas series with a unique rank for every row in df
+
+See Also
+--------
+# List ranking algorithms here!
+
+"""
+# # do the actual stuff
+df = df.set_index([
+    # 'cluster',
+    'earn_tot_mean_1315',
+    'duration_days_0',
+    'id'])
+df.sort_index(inplace=True)
+df.reset_index(inplace=True)
+df.index.name = 'rank'
+# # np.sum(df1.index.duplicated())
+df.reset_index(inplace=True)
+df.set_index('id', inplace=True)
+
+# return rank
+
 # %%
+# def create_bins (df, bin_method, bin_size, bin_number):
+"""
+Assigns records to bins, given a ranking and specified number or size of bins
 
 
 
+Parameters
+----------
+df: DataFrame
+    Must have T vs C column and ranks
+bin_method: String
+    "size" or "count"
+bin_size: int
+    Max or min?
+bin_count: int
+    Max or min
+
+
+Returns
+-------
+bin_col: Series
+
+"""
+# # do the actual stuff
+
+# %%
+df_T = df.loc[df["Group1"] == "T1"].copy()
+df_C = df.loc[df["Group1"] == "C1"].copy()
+
+df_T["t_centile"], bins = pd.qcut(
+    df_T["rank"], 100, retbins=True, labels=False)
+
+df_C["t_centile"] = pd.cut(df_C["rank"], bins, labels=range(len(bins) - 1))
+# return bin_col
+
+
+# def add_weights ()
+"""Create T, C, and X groups for all periods
+# In:       Whatever we've got already
+# Out:     [periods (1, 0, -)]
+
+
+# def get_weights_all_periods:
+# In:       df [periods] [weighting cols] weighting_score_fn, bin_fn
+# Out:     [period weight columns]
+
+# def get_weights:
+# In:      period, [weighting_cols], weighting_score_fn, bin_fn
+# Out:     weight, t_bin
+
+"""
+# 	1. Create ranks
+# 	2. Create T and C bins based on ranks
+# 	3. Add weights
+t_count = df_T["t_centile"].count()
+t_centile_weights = df_T.groupby(["t_centile"])[
+    "rank"].count().divide(t_count)
+df_T["weight"] = 1
+c_count = df_C["t_centile"].count()
+
+c_t_centile_weights = df_C.groupby(
+    ["t_centile"])["t_centile"].count().divide(c_count)
+c_weights = t_centile_weights / c_t_centile_weights
+df_C["weight"] = df_C["t_centile"].map(c_weights)
+
+df = df_T.append(df_C)
 
 # %% [markdown]
 # ## Create weighted versions of background and outcome columns
 # %%
+numeric_cols = [
+    col for col in df.columns.tolist() if col.startswith(("earn_", "sw_pay_"))
+]
+
 for col in numeric_cols:
     df["w_" + col] = df[col] * df["weight"]
 
@@ -210,6 +204,14 @@ df["w_impact_income_diff"] = df["w_income_diff"] - df["income_diff"]
 # %% [markdown]
 # ## Descriptive statistics
 
+# %%
+# import sklearn.cross_validation.Bootstrap as bootstrap
+# import scipy
+# for col in w_numeric_cols:
+#     print(col,
+#     df.groupby('Group1')[col].apply(
+#         lambda x:bootstrap.ci(data=x, statfunction=scipy.mean)))
+
 # %% [markdown]
 # ### Weighting impact
 
@@ -220,9 +222,9 @@ df[["w_impact_sw_pay_mean_1315", "w_impact_earn_tot_mean_1315"]].hist(
 
 # %%
 f, ax = plt.subplots(dpi=1000)
-sns.scatterplot(x="bin", y="weight", hue="Group1", data=df)
+sns.scatterplot(x="t_centile", y="weight", hue="Group1", data=df)
 fig = ax.get_figure()
-fig.savefig("images/weights_by_bin.png")
+fig.savefig("images/weights_by_centile.png")
 ax.plot()
 
 # %% [markdown]
@@ -315,4 +317,4 @@ fig.savefig("images/w_earnings_vs_sw_pay.png")
 ax.plot()
 
 
-# %%
+#%%
