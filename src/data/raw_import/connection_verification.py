1b7e1adb61907afd7737b7c77c2e42f6257aab42
Hugh Cronin <HughCronin@welfare.irlgov.ie>
2020-01-31 14:56:16 +0000

    rewrote queries to more generic form, and tested

--- src/data/raw_import/connection_verification.py
+++ src/data/raw_import/connection_verification.py
@@ -12,6 +12,34 @@ join ists_personal p
 on c.personal_id=p.id
 where lr_date = '2020-01-03' and occupation=10"""
 sql_ists = pd.read_sql(query, con=engine)
+sql_ists.head()
+# %%
+
+query="""select * from payments
+where AMOUNT='500'"""
+sql_pay = pd.read_sql(query, con=engine)
+sql_pay.head()
+# %%
+
+query="""select * from payments
+where QTR='2014Q1'"""
+sql_pay = pd.read_sql(query, con=engine)
+sql_pay.head()
+#%%
+query="""select * from earnings where 
+CON_YEAR=2007 and 
+EARNINGS_AMT=10000 """
+sql_earnings = pd.read_sql(query, con=engine)
+sql_earnings.head()
+# %%
+query="""select * from les"""
+sql_les = pd.read_sql(query, con=engine)
+
+# %%
+query="""select * from Penalties"""
+sql_penalties = pd.read_sql(query, con=engine)
+# %%
+
 # %%
 select *                                              "+
                      "    from ists_claims c                                "+
@@ -21,45 +49,3 @@ select *                                              "+
                      con=engine
 sql_ists = pd.read_sql(query, con=engine)
 
-sql_ists = pd.read_sql("select *                                              "+
-                     "    from ists_claims c                                "+
-                     "    join ists_personal p                              "+
-                     "        on c.personal_id=p.id                         "+
-                     "    where lr_date = '2020-01-03' and occupation=10    ",
-                     con=engine)
-#print(sql_ists.head(3))
-
-# %%
-
-query="""select * from payments"""
-sql_pay = pd.read_sql(query, con=engine)
-
-sql_pay = pd.read_sql("select *                    "+
-                     "    from payments           "+
-                     "    where ppsn='7035482E'   ",
-                     con=engine)
-#print( sql_DF)
-# %%
-
-query="""select * from earnings"""
-sql_earnings = pd.read_sql(query, con=engine)
-
-sql_earnings = pd.read_sql("select *                          "+
-                     "    from earnings                 "+
-                     "    where RSI_NO='0062871I'       ",
-                     con=engine)
-#print( sql_DF)
-# %%
-query="""select * from les"""
-sql_les = pd.read_sql(query, con=engine)
-
-
-sql_les = pd.read_sql("select *                          "+
-                     "    from les                      ",                    
-                     con=engine)
-#print( sql_DF)
-# %%
-query="""select * from Penalties"""
-sql_penalties = pd.read_sql(query, con=engine)
-#print( sql_DF)
-# %%
