9fa03b6a20a8a11d63d7dc4449e4ed731f14995b
Ciaran Judge <45339268+ciaranjudge@users.noreply.github.com>
2020-03-10 11:22:44 +0000

    Setting up first tests

--- tests/data/test_persistence_helpers.py
+++ tests/data/test_persistence_helpers.py
@@ -1,39 +1,5 @@
-
-    import evaluation_jp.data.import_helpers
-    import evaluation_jp.data.persistence_helpers
-    import evaluation_jp.features.build_features
-    import evaluation_jp.features.metadata_helpers
-    import evaluation_jp.features.selection_helpers
-    import evaluation_jp.models.model
-    import evaluation_jp.models.slice
-    import evaluation_jp.models.period
-    
-
-# returned_df = get_ists_claims(
-#     pd.Timestamp("2020-01-03"),
-#     lr_flag=True,
-#     # columns=["lr_code", "clm_comm_date", 'lr_flag'],
-#     ids=["0070688N", "0200098K"],
-# )
-# returned_df.describe()
-
-#%%
-# test
-
-
-# context1 = ssl._create_unverified_context() # add context below
-# url_sector = "https://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/LRM02"
-# try:
-#     with urlopen(url_sector) as f:
-#         LRM02 = json.load(f)
-#         LR_monthly = create_dataframe(LRM02)
-# except:
-#     with urlopen(url_sector, context=context1) as f:
-#         LRM02 = json.load(f)
-#         LR_monthly = create_dataframe(LRM02)
-
-
-
+import evaluation_jp.data.persistence_helpers
+ 
 # # %%
 # from typing import ClassVar, List, Set, Dict, Tuple, Optional
 # from dataclasses import dataclass, Field, fields, asdict, astuple, is_dataclass
