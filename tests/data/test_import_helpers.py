cb7f0ffaf4c735b2fe92d785a1c07e40fd94ee4a
Ciaran Judge <45339268+ciaranjudge@users.noreply.github.com>
2020-03-10 12:37:35 +0000

    Added test_get_datetime_cols()

--- tests/data/test_import_helpers.py
+++ tests/data/test_import_helpers.py
@@ -1,6 +1,29 @@
-import evaluation_jp.data.import_helpers
+from evaluation_jp.data.import_helpers import get_datetime_cols, get_ists_claims
 
-# TODO test get_datetime_cols()
+
+def test_get_datetime_cols():
+    test_inputs = ["les", "ists_personal", "jobpath_referrals"]
+    test_outputs = {
+        table_name: set(get_datetime_cols(table_name)) for table_name in test_inputs
+    }
+    expected_outputs = {
+        "les": set(["start_date", "start_week"]),
+        "ists_personal": set(["date_of_birth"]),
+        "jobpath_referrals": set(
+            [
+                "referral_date",
+                "jobpath_start_date",
+                "jobpath_end_date",
+                "cancellation_date",
+                "pause_date",
+                "resume_date",
+                "ppp_agreed_date",
+            ]
+        ),
+    }
+    assert test_outputs == expected_outputs
+
+
 # TODO test get_clusters()
 # TODO test get_ists_claims()
 # TODO test get_les_data()
@@ -16,7 +39,4 @@ import evaluation_jp.data.import_helpers
 #     ids=["0070688N", "0200098K"],
 # )
 # returned_df.describe()
-
-
-
 
