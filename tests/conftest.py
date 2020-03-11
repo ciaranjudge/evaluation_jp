3affc48b294beb08e381ed2387d3957d2be0a30b
Ciaran Judge <45339268+ciaranjudge@users.noreply.github.com>
2020-03-11 15:03:59 +0000

    Sketched stub for fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+# import pandas as pd
+# import pytest
+# import sqlalchemy as sa
+
+# @pytest.fixture(scope="module")
+# engine = sa.create_engine(
+#     "sqlite:///\\\\cskma0294\\F\\Evaluations\\data\\wwld.db", echo=False
+# )
+# insp = sa.engine.reflection.Inspector.from_engine(engine)
