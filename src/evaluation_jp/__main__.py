e02314d2a5710322016d0ea8e5dce22e144659f7
Ciaran Judge <45339268+ciaranjudge@users.noreply.github.com>
2020-05-18 23:36:38 +0100

    Update __main__ to create slices and periods

--- src/evaluation_jp/__main__.py
+++ src/evaluation_jp/__main__.py
@@ -20,7 +20,10 @@ from evaluation_jp.features import (
     ClaimDurationEligible,
     OnLES,
     OnJobPath,
+    JobPathStartedEndedSamePeriod,
     EligiblePopulation,
+    JobPathStarts,
+    EvaluationGroup,
 )
 from evaluation_jp.data import ModelDataHandler
 
@@ -29,7 +32,7 @@ evaluation_model = EvaluationModel(
     data_handler=ModelDataHandler(
         database_type="sqlite",
         location="//cskma0294/f/Evaluations/JobPath",
-        name="test",
+        name="jobpath_evaluation",
     ),
     population_slice_generator=PopulationSliceGenerator(
         start=pd.Timestamp("2016-01-01"),
@@ -39,13 +42,12 @@ evaluation_model = EvaluationModel(
             pd.Timestamp("2016-01-01"): SetupSteps(
                 steps=[
                     LiveRegisterPopulation(
-                        columns=[
-                            "lr_code",
-                            "clm_comm_date",
-                            "JobPath_Flag",
-                            "JobPathHold",
-                            "date_of_birth",
-                        ]
+                        columns_by_type={
+                            "lr_code": "category",
+                            "clm_comm_date": "datetime64",
+                            "JobPath_Flag": "boolean",
+                            "date_of_birth": "datetime64",
+                        }
                     ),
                     AgeEligible(
                         date_of_birth_col="date_of_birth", max_eligible={"years": 60}
@@ -79,11 +81,18 @@ evaluation_model = EvaluationModel(
         setup_steps_by_date={
             pd.Timestamp("2016-01-01"): SetupSteps(
                 steps=[
-                    StartingPopulation(
-                        starting_pop_col="evaluation_group", starting_pop_val="C"
+                   StartingPopulation(
+                        eligible_from_pop_slice_col="eligible_population",
+                        eligible_from_previous_period_col="evaluation_group",
+                        starting_pop_label="C",
                     ),
                     LiveRegisterPopulation(
-                        columns=["lr_code", "clm_comm_date", "JobPath_Flag", "JobPathHold",],
+                        columns_by_type={
+                            "lr_code": "category",
+                            "clm_comm_date": "datetime64",
+                            "JobPath_Flag": "boolean",
+                            "JobPathHold": "boolean",
+                        },
                         starting_pop_col="eligible_population",
                     ),
                     ClaimCodeEligible(code_col="lr_code", eligible_codes=["UA", "UB"]),
@@ -92,17 +101,25 @@ evaluation_model = EvaluationModel(
                     ),
                     OnLES(assumed_episode_length={"years": 1}, how="start"),
                     OnLES(assumed_episode_length={"years": 1}, how="end"),
+                    JobPathStartedEndedSamePeriod(),
                     EligiblePopulation(
                         eligibility_criteria={
+                            "on_live_register": True,
                             "claim_code_eligible": True,
                             "claim_duration_eligible": True,
                             "on_les_at_start": False,
                             "on_les_at_end": False,
                             "JobPathHold": False,
+                            "jobpath_started_and_ended": False,
                         }
                     ),
-                    # *"eligible_population" -> JobPath starts -> "evaluation_group" = "T" else "C"
-                    # ?What to do about JP starts that finish within same period?
+                    JobPathStarts(),
+                    EvaluationGroup(
+                        eligible_col="eligible_population",
+                        treatment_col="jobpath_starts",
+                        treatment_label="T",
+                        control_label="C",
+                    ),
                 ]
             )
         },
@@ -113,4 +130,6 @@ evaluation_model = EvaluationModel(
     # )
 )
 
+
 evaluation_model.add_population_slices()
+evaluation_model.add_treatment_periods()
