df42119c5e79132d1e26cf5ebc4cc12ae6869b64
Ciaran Judge <45339268+ciaranjudge@users.noreply.github.com>
2020-05-25 16:05:21 +0100

    Add skeleton for features._longitudinal_data

---
+++ src/evaluation_jp/features/_longitudinal_data.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+
+from evaluation_jp.data import get_earnings, get_sw_payments, get_ists_claims
+
+
+# //TODO Add sex, marital status, location to longitudinal data
+
+# //TODO LR status by quarter (as share)
+
+# //TODO Add earnings (quarterised!) to longitudinal data
+
+# //TODO Add SW payments (by type) to longitudinal data
+
+# //TODO Defeat outliers! Add feature scaling!
