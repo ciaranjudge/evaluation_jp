8b0ae509f43e3d4d06eb541eea2e775ad716281f
Ciaran Judge <CiaranJudge@welfare.irlgov.ie>
2020-09-02 15:08:44 +0100

    Enable sqlite in DataHandler, add parametrized tests

--- src/evaluation_jp/data/_data_handler.py
+++ src/evaluation_jp/data/_data_handler.py
@@ -1,20 +1,19 @@
 # %%
 # Standard library
 import abc
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 
 
 # External packages
 import pandas as pd
+from pandas.errors import EmptyDataError
 
 # import pyodbc
 import sqlalchemy as sa
 
 # Local packages
 from evaluation_jp.data.sql_utils import *
-
-# if TYPE_CHECKING:
 from evaluation_jp import DataID, DataParams
 
 
@@ -69,30 +68,44 @@ class SQLDataHandler(DataHandler):
     model_schema: str
 
     def __post_init__(self):
-        """In sqlserver, create the schema model_schema if it doesn't exist
+        """Create the database schema model_schema using engine if it doesn't exist.
+        # ...unless using sqlite engine, in which case do nothing.
         """
-        # //TODO Make schemas work properly in SQLite
-        if self.model_schema not in self.engine.dialect.get_schema_names(self.engine):
-            self.engine.execute(sa.schema.CreateSchema(self.model_schema))
-    
+        if not self.engine.dialect.name == "sqlite":
+            if self.model_schema not in self.engine.dialect.get_schema_names(
+                self.engine
+            ):
+                self.engine.execute(sa.schema.CreateSchema(self.model_schema))
+
     def table_exists(self, table_name):
-        if table_name in sa.inspect(self.engine).get_table_names(
-            schema=self.model_schema
-        ):
+        if self.engine.dialect.name == "sqlite":
+            table_name = f"{self.model_schema}.{table_name}"
+            schema = "main"
+        else:
+            table_name = table_name
+            schema = self.model_schema
+        if table_name in sa.inspect(self.engine).get_table_names(schema):
             return True
         else:
             return False
 
-    def read(
-        self, data_params: DataParams, data_id: DataID = None
-    ):
-        """Load dataframe from records in `table` matching `id`
+    def read(self, data_params: DataParams, data_id: DataID = None):
+        """Loads dataframe from records in data_params.table_name with optional data_id.
+        Fixes dtypes based on data_params.columns_by_type.
         """
         if self.table_exists(data_params.table_name):
-            query = f"""\
-                SELECT * 
-                    FROM {self.model_schema}.{data_params.table_name}
-                """
+            # SQLite doesn't really understand schemas - it just thinks they're in table name.
+            # But it can't cope with a dot in table name unless wrapped in [ ]
+            if self.engine.dialect.name == "sqlite":
+                 query = f"""\
+                    SELECT * 
+                        FROM [{self.model_schema}.{data_params.table_name}]
+                    """
+            else:
+                query = f"""\
+                    SELECT * 
+                        FROM {self.model_schema}.{data_params.table_name}
+                    """
             if sql_data_id := get_sql_data_id(data_id):
                 query += sql_where_clause_from_dict(sql_data_id)
 
@@ -109,14 +122,17 @@ class SQLDataHandler(DataHandler):
         else:
             raise TableNotFoundError
 
-        # //TODO Implement read from archive (in memory and into live database)
-
     def _delete(self, table_name, sql_data_id=None):
         # If the table exists, delete any previous rows with this data_id
         if self.table_exists(table_name):
-            query = f"""\
-                DELETE FROM {self.model_schema}.{table_name}
-            """
+            if self.engine.dialect.name == "sqlite":
+                query = f"""\
+                    DELETE FROM [{self.model_schema}.{table_name}]
+                """
+            else:
+                query = f"""\
+                    DELETE FROM {self.model_schema}.{table_name}
+                """
             if sql_data_id:
                 query += sql_where_clause_from_dict(sql_data_id)
             with self.engine.connect() as conn:
@@ -132,19 +148,36 @@ class SQLDataHandler(DataHandler):
             for idx in data_id_indexes:
                 idx = idx if isinstance(idx, list) else [idx]
                 try:
-                    query = f"""\
-                        CREATE INDEX idx_{'_'.join(i for i in idx)}
-                        ON {self.model_schema}.{table_name} ({', '.join(i for i in idx)})
-                        """
+                    if self.engine.dialect.name == "sqlite":
+                        query = f"""\
+                            CREATE INDEX idx_{'_'.join(i for i in idx)}
+                            ON [{self.model_schema}.{table_name}] ({', '.join(i for i in idx)})
+                            """
+                    else:
+                        query = f"""\
+                            CREATE INDEX idx_{'_'.join(i for i in idx)}
+                            ON {self.model_schema}.{table_name} ({', '.join(i for i in idx)})
+                            """
                     with self.engine.connect() as conn:
                         conn.execute(query)
                 except:
                     pass
 
     def write(self, data, data_params, data_id=None, use_index=True):
+        """Write dataframe `data` to `data_params.table_name`. 
+        If there's a data_id, add the data_id fields as columns in the table.
+        If data already exists with same data_params.table_name and optional data_id, overwrite it.
+        If use_index is True, add the index columns of data to the table.
+        Raise EmptyDataError if passed an empty dataframe.
+        """
         sqldata = data.copy()
+
+        if sqldata.empty:
+            raise EmptyDataError
+
         if use_index:
             sqldata.reset_index()
+
         for col in data.columns:
             if "period" in str(data[col].dtype):
                 sqldata[col] = sqldata[col].astype(str)
@@ -153,10 +186,15 @@ class SQLDataHandler(DataHandler):
             self._delete(data_params.table_name, sql_data_id)
             for k, v in sql_data_id.items():
                 sqldata[k] = v
-
+            if self.engine.dialect.name == "sqlite":
+                table_name = f"{self.model_schema}.{data_params.table_name}"
+                schema = "main"
+            else:
+                table_name = data_params.table_name
+                schema = self.model_schema
             sqldata.to_sql(
-                data_params.table_name,
-                schema=self.model_schema,
+                table_name,
+                schema=schema,
                 con=self.engine,
                 if_exists="append",
                 index=False,
@@ -178,19 +216,16 @@ def populate(
     data_handler: DataHandler = None,
     rebuild: bool = False,
 ):
-    if data_handler is not None:
-        if not rebuild:
-            try:
-                data = data_handler.read(data_params, data_id)
-            except DataHandlerError:
-                rebuild = True
-        if rebuild:
-            data = data_params.setup_steps.run(data_id=data_id, data=init_data)
-            data = data_params.set_datatypes(data)
+    if data_handler is not None and not rebuild:
+        try:
+            data = data_handler.read(data_params, data_id)
+        except DataHandlerError:
+            rebuild = True
+    if data_handler is None or rebuild:
+        data = data_params.setup_steps(data_id).run(data_id=data_id, data=init_data)
+        data = data_params.columns_by_type.set_datatypes(data)
+        if data_handler is not None:
             data_handler.write(data, data_params, data_id)
-    else:
-        data = data_params.setup_steps.run(data_id=data_id, data=init_data)
-        data = data_params.set_datatypes(data)
     return data
 
 
@@ -200,24 +235,3 @@ def populate(
 #     # def copy_existing(cls, old_data_path, new_data_path, rebuild_all):
 #     #     # Make copy of old database at new_data_path
 #     #     pass
-
-# * Yay I've fixed this important issue #101!
-
-
-#     if self.table_exists(table_name):
-#         # max_data_version = get max_data_version using sql_id
-#     else:
-#         max_data_version = 0
-
-#    if data_version is not None:
-#         # Is 0 < data_version <= max_data_version?
-#         #   Yay! Let's keep it!
-#         # else:
-#         #   raise NotAThingError
-#     else:
-#         data_version = max_data_version
-
-#     sql_id["data_version"] = data_version
-
-#     return sql_id
-
--- tests/data/test__data_handler.py
+++ tests/data/test__data_handler.py
@@ -4,64 +4,83 @@ from IPython.display import display
 import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
+import pytest
 import sqlalchemy as sa
 
 from evaluation_jp import (
     ColumnsByType,
-    DataHandler,
     SQLDataHandler,
+    populate,
     PopulationSliceID,
     PopulationSliceDataParams,
+    SetupStep,
+    SetupSteps,
     TreatmentPeriodID,
     TreatmentPeriodDataParams,
 )
 from evaluation_jp.data.sql_utils import *
 
 
-# def test__SQLDataHandler__init__sqlite(tmpdir):
-#     """Simple test to make sure everything gets initiated correctly
-#     """
 
-#     engine = sqlite_engine(tmpdir, "test_db")
-
-#     data_handler = SQLDataHandler(engine=engine, model_schema="test_schema")
-
-#     assert data_handler.engine.dialect.name == "sqlite"
-#     assert data_handler.model_schema == "test_schema"
+class TestStep(SetupStep):
+    def run(self, data=None, data_id=None):
+        return pd.DataFrame(
+            np.random.randint(0, 100, size=(100, 4)), columns=list("ABCD")
+        )
 
 
-def test__SQLDataHandler__init__sqlserver(tmpdir):
-    """Simple test to make sure everything gets initiated correctly
-    """
-    engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
-
-    data_handler = SQLDataHandler(engine=engine, model_schema="test_schema")
-
-    assert data_handler.engine.dialect.name == "mssql"
-    assert data_handler.model_schema == "test_schema"
-
-
-# * No vs simple vs complex data_id, sqlserver vs sqlite, +/- use_index = 12 tests
-
+test_setup_steps_by_date = {pd.Timestamp("2016-01-01"): SetupSteps([TestStep])}
 
 population_slice_data_params = PopulationSliceDataParams(
     columns_by_type=ColumnsByType(
         data_columns_by_type={col: "int32" for col in list("ABCD")}
     ),
-    setup_steps_by_date=None,
+    setup_steps_by_date=test_setup_steps_by_date,
 )
 population_slice_id = PopulationSliceID(date=pd.Timestamp("2016-01-01"))
 
 
-def test__DataHandler__write_new__PopulationSlice__with_data_id__no_index__sqlserver():
+@pytest.mark.parametrize("sql_dialect", ["sqlite", "mssql"])
+def test__SQLDataHandler__init(tmpdir, sql_dialect):
+    """Simple test to make sure everything gets initiated correctly
+    """
+    if sql_dialect == "sqlite":
+        engine = sqlite_engine(tmpdir, "test_db")
+    if sql_dialect == "mssql":
+        engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
+
+    data_handler = SQLDataHandler(engine=engine, model_schema="test_schema")
+
+    assert data_handler.engine.dialect.name == sql_dialect
+    assert data_handler.model_schema == "test_schema"
+
+
+# +/- use_index = 12 tests
+
+
+@pytest.mark.parametrize("sql_dialect", ["sqlite", "mssql"])
+def test__DataHandler__write_new__PopulationSlice__with_data_id__no_index(
+    tmpdir, sql_dialect
+):
     """Write dataframe with *new* PopulationSlice data_id to SQL test_schema.population_slice
     """
-    engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
     data_params = population_slice_data_params
     data_id = population_slice_id
+
+    schema = "test_schema"
+
+    if sql_dialect == "sqlite":
+        engine = sqlite_engine(tmpdir, "test")
+        sql_table_name = f"{schema}.{data_params.table_name}"
+        sql_schema = "main"
+    if sql_dialect == "mssql":
+        engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
+        sql_table_name = data_params.table_name
+        sql_schema = schema
+
     with engine.connect() as con:
         # Setup
-        data_handler = SQLDataHandler(engine=con, model_schema="test_schema")
+        data_handler = SQLDataHandler(engine=con, model_schema=schema)
 
         # Write
         data = pd.DataFrame(
@@ -70,7 +89,8 @@ def test__DataHandler__write_new__PopulationSlice__with_data_id__no_index__sqlse
         data_handler.write(data, data_params, data_id, use_index=False)
 
         # Read back data from SQL following write
-        df = pd.read_sql_table("population_slice", con=con, schema="test_schema")
+
+        df = pd.read_sql_table(sql_table_name, con=con, schema=sql_schema)
 
     # Results should be same as original data passed to data_handler.write()
     results = df.loc[df["data_id_date"] == str(data_id.date.date())].drop(
@@ -79,16 +99,29 @@ def test__DataHandler__write_new__PopulationSlice__with_data_id__no_index__sqlse
     assert_frame_equal(data.describe(), results.describe())
 
 
-def test__DataHandler__write_overwrite__PopulationSlice__with_data_id__no_index__sqlserver():
+@pytest.mark.parametrize("sql_dialect", ["sqlite", "mssql"])
+def test__DataHandler__write_overwrite__PopulationSlice__with_data_id__no_index(
+    tmpdir, sql_dialect
+):
     """Write dataframe with *existing* PopulationSlice data_id to SQL test_schema.population_slice
     """
-    engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
     data_params = population_slice_data_params
     data_id = population_slice_id
 
+    schema = "test_schema"
+
+    if sql_dialect == "sqlite":
+        engine = sqlite_engine(tmpdir, "test")
+        sql_table_name = f"{schema}.{data_params.table_name}"
+        sql_schema = "main"
+    if sql_dialect == "mssql":
+        engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
+        sql_table_name = data_params.table_name
+        sql_schema = schema
+
     with engine.connect() as con:
         # Setup data_handler
-        data_handler = SQLDataHandler(engine=con, model_schema="test_schema")
+        data_handler = SQLDataHandler(engine=con, model_schema=schema)
 
         # First write
         data_1 = pd.DataFrame(
@@ -102,8 +135,9 @@ def test__DataHandler__write_overwrite__PopulationSlice__with_data_id__no_index_
         )
         data_handler.write(data_2, data_params, data_id, use_index=False)
 
-        # Read back data from SQL following second write
-        df = pd.read_sql_table("population_slice", con=con, schema="test_schema")
+        # Read back data from SQL following write
+
+        df = pd.read_sql_table(sql_table_name, con=con, schema=sql_schema)
 
     # Results should be same as (2nd time around) data passed to data_handler.write()
     results = df.loc[df["data_id_date"] == str(data_id.date.date())].drop(
@@ -112,15 +146,25 @@ def test__DataHandler__write_overwrite__PopulationSlice__with_data_id__no_index_
     assert_frame_equal(data_2.describe(), results.describe())
 
 
-def test__DataHandler__read_existing__PopulationSlice__with_data_id__no_index__sqlserver():
+@pytest.mark.parametrize("sql_dialect", ["sqlite", "mssql"])
+def test__DataHandler__read_existing__PopulationSlice__with_data_id__no_index(
+    tmpdir, sql_dialect
+):
     """Read dataframe with *existing* PopulationSlice data_id from SQL test_schema.population_slice
     """
-    engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
     data_params = population_slice_data_params
     data_id = population_slice_id
+
+    schema = "test_schema"
+
+    if sql_dialect == "sqlite":
+        engine = sqlite_engine(tmpdir, "test")
+    if sql_dialect == "mssql":
+        engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
+
     with engine.connect() as con:
         # Setup
-        data_handler = SQLDataHandler(engine=con, model_schema="test_schema")
+        data_handler = SQLDataHandler(engine=con, model_schema=schema)
 
         # Write
         data = pd.DataFrame(
@@ -128,69 +172,28 @@ def test__DataHandler__read_existing__PopulationSlice__with_data_id__no_index__s
         )
         data_handler.write(data, data_params, data_id, use_index=False)
 
-        # Read 
+        # Read
         results = data_handler.read(data_params, data_id)
 
     assert_frame_equal(data.describe(), results.describe())
+
 
 # //TODO test read when table doesn't exist
 # //TODO test read when table exists but data_id doesn't exist
 # //TODO test index creation
 
-# # def test__DataHandler__read(fixture__population_slice, tmpdir):
 
-# #     data_path = f"sqlite:///{tmpdir}/test_DataHandler.db"
-# #     population_slice = fixture__population_slice
-# #     data_handler = DataHandler(data_path)
-# #     data_handler.write(
-# #         data_type=population_slice.class_name,
-# #         data_id=population_slice.id,
-# #         data=population_slice.data,
-# #         index=False,
-# #     )
-# #     display(population_slice.data)
-# #     results = data_handler.read(
-# #         data_type=population_slice.class_name, data_id=population_slice.id,
-# #     )
-# #     display(results)
-# #     assert results.shape == population_slice.data.shape
+def test__populate__no_data_handler():
+    data_params = population_slice_data_params
+    data_id = population_slice_id
+    results = populate(data_params, data_id)
+    assert results.shape == (100, 4)
+    assert set(results.columns) == set(list("ABCD"))
 
 
-# # # def test__DataHandler__run__new(
-# # #     fixture__setup_steps_by_date, fixture__population_slice_generator, tmpdir
-# # # ):
-# # #     data_path = f"sqlite:///{tmpdir}/test_DataHandler.db"
-# # #     data_handler = DataHandler(data_path)
-# # #     population_slice_generator = fixture__population_slice_generator
-# # #     results = {
-# # #         population_slice.id: population_slice
-# # #         for population_slice in population_slice_generator.run(data_handler)
-# # #     }
-# # #     key = PopulationSliceID(date=pd.Timestamp("2016-07-01", freq="QS-JAN"))
-
-# # #     assert results[key].data.shape == (90, 5,)
-
-
-# # # def test__DataHandler__run__existing(
-# # #     fixture__setup_steps_by_date, fixture__population_slice_generator, tmpdir
-# # # ):
-# #     data_path = f"sqlite:///{tmpdir}/test_DataHandler.db"
-# #     data_handler = DataHandler(data_path)
-# #     population_slice_generator = fixture__population_slice_generator
-# #     # First iteration should run setup_steps then write to storage
-# #     first_population_slices = {
-# #         population_slice.id: population_slice
-# #         for population_slice in population_slice_generator.run(data_handler)
-# #     }
-# #     # Second iteration should just read from storage
-# #     second_population_slices = {
-# #         population_slice.id: population_slice
-# #         for population_slice in population_slice_generator.run(data_handler)
-# #     }
-# #     key = PopulationSliceID(date=pd.Timestamp("2016-07-01", freq="QS-JAN"))
-
-# #     assert (
-# #         len(first_population_slices[key].data)
-# #         == len(second_population_slices[key].data)
-# #     )
-
+# @pytest.mark.parametrize("data_params", "data_id", "init_data")
+# @pytest.mark.parametrize("rebuild", [True, False])
+# @pytest.mark.parametrize("overwrites")
+@pytest.mark.parametrize("sql_dialect", ["sqlite", "mssql", "no_database"])
+def test__populate__new(tmpdir, sql_dialect):
+    pass
