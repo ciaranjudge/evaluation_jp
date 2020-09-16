b658a5c75b6dafe55de3ee2abb8a684dfd6560e0
Ciaran Judge <CiaranJudge@welfare.irlgov.ie>
2020-09-16 13:27:48 +0100

    Tidy geat_earnings_contributions_data() and add docstring

--- src/evaluation_jp/data/sql_importers.py
+++ src/evaluation_jp/data/sql_importers.py
@@ -11,14 +11,13 @@ def get_edw_customer_details(
     reference_date: pd.Timestamp = pd.Timestamp.now(),
 ) -> pd.DataFrame:
     """Generic EDW SCD import function
-    Requires: a pd.Series or pd.DataFrame of ppsns_to_lookup (from ppsn_column_name )
+    Requires: a pd.Series or pd.DataFrame of ppsns_to_lookup (from ppsn_column_name, default "ppsn")
     Returns time-specific values at reference_date (or now if reference_date omitted)
     for lookup_columns (or all columns if no columns specified)
     """
-    server = "CSKMA0400\\STATS1"
     edw_database = "Stat"
     edw_schema = "edw"
-    tempdb_engine = sqlserver_engine(server, "tempdb")
+    tempdb_engine = sqlserver_engine("CSKMA0400\\STATS1", "tempdb")
     with temp_table_connection(
         tempdb_engine, ppsns_to_lookup, "ppsn_table"
     ) as temp_table_con:
@@ -45,25 +44,27 @@ def get_earnings_contributions_data(
     start_year: int = None,
     end_year: int = None,
 ) -> pd.DataFrame:
+    """Get earnings and contributions data from CRS for DataFrame or Series of ppsns_to_lookup.
+    ppsn_column_name for ppsns_to_lookup is "ppsn" by default.
+    Will return lookup_columns from earnings/contributions database if specified, otherwise all columns.
+    Optionally use start_year and/or end_year to restrict range of years returned.
+    """
     # Get BOMi ids for ppsns
     ids_to_lookup = get_edw_customer_details(
         ppsns_to_lookup, ppsn_column_name, lookup_columns=["customer_id"]
     )
-    # Create temp_customerid_year table
-    server = "CSGPC-BPRD-SQ06\\PA1"
-    tempdb_engine = sqlserver_engine(server, "tempdb")
+    # Use table with ppsns and BOMi ids to lookup earnings and contributions data
+    tempdb_engine = sqlserver_engine("CSGPC-BPRD-SQ06\\PA1", "tempdb")
     with temp_table_connection(
         tempdb_engine, ids_to_lookup, "##id_table"
     ) as temp_table_con:
-        query_columns = (
-            unpack([f"concrete_cons.{col}" for col in lookup_columns])
-            if lookup_columns is not None
-            else "concrete_cons.*"
-        )
         query = f"""\
             SELECT  ##id_table.{ppsn_column_name},
                     abstract_cons.year,
-                    {query_columns}
+                    {unpack([f"concrete_cons.{col}" for col in lookup_columns])
+                        if lookup_columns is not None
+                        else "concrete_cons.*"
+                    }
             FROM    ##id_table
                     INNER JOIN dbbomcrs.employment.abstract_contribution_year AS abstract_cons
                         ON ##id_table.customer_id = abstract_cons.customer_id
@@ -77,34 +78,9 @@ def get_earnings_contributions_data(
             ORDER BY {ppsn_column_name}, year  
         """
         return pd.read_sql(query, temp_table_con)
-                    
 
 
-    #
-
-
-# *Get vital statistics for a reference date [<- PopulationSlice]
-#
-
-# *Death check
-
-
-# # STATS1 server tempdb
-# stats_tempdb_engine = st.server_connection(server="CSKMA0400\STATS1", db="tempdb")
-# # STATS1 server ia db
-# stats_ia_db_engine = st.server_connection(server="CSKMA0400\STATS1", db="ia")
-# # PA1 server tempdb
-# pa1_tempdb_engine = st.server_connection(server="CSGPC-BPRD-SQ06\PA1", db="tempdb")
-# # PA1 server dbBomCrs
-# pa1_bomcrs_db_engine = st.server_connection(server="CSGPC-BPRD-SQ06\PA1", db="dbBomCrs")
-# # PA1 server dbBommain
-# pa1_bommain_db_engine = st.server_connection(
-#     server="CSGPC-BPRD-SQ06\PA1", db="dbBommain"
-# )
-
-
-# BOMi IDs from PPS numbers
-# Vital statistics from SCD database
+# //TODO Data cleaning for earnings data 
 
 
 # * Separately look up deceased status from SCD table
