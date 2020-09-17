6e961fb97202395931c887888de47af90d42794c
croninhugh <HughCronin@welfare.irlgov.ie>
2020-09-17 16:52:32 +0100

    generate and import dummy data

--- src/evaluation_jp/models/_disability_setup_steps.py
+++ src/evaluation_jp/models/_disability_setup_steps.py
@@ -1,14 +1,23 @@
 
 
-Use setup steps as model, write function to estimate shares from population slice, taking DA recipiency x (param) months subsequent to:
-1. start v referred and no start
-2. Start v control (not referred)
-3. Referred v control
+# Use setup steps as model, write function to estimate shares from population slice, taking DA recipiency x (param) months subsequent to:
+# 1. start v referred and no start
+# 2. Start v control (not referred)
+# 3. Referred v control
 
-dummy data step
-start_ref=data['JobPath_start' data['JobPath_ref'] 1,0
-start_ctrl=data['JobPath_start']1,0
-ref_ctrl=data['JobPath_ref']1,0
+# dummy data step
+# start_ref=data['JobPath_start' data['JobPath_ref'] 1,0
+# start_ctrl=data['JobPath_start']1,0
+# ref_ctrl=data['JobPath_ref']1,0
+from src\evaluation_jp\data\sql importers.py, import *
+
+import pandas as pd
+
+from pathlib import Path
+
+dummy_data_folder=Path("//cskma0294/F/Evaluations/JobPath/dummy_data")
+dummy_data_file="jp_dummy_data_dis.csv"
+dummy = pd.read_csv(dummy_data_folder/dummy_data_file)
 
 def exit_to_da(df, interval, criteria):
 """function to establish DA recipiency at some interval, comparing this share for mutually exclusive cohorts""" 
